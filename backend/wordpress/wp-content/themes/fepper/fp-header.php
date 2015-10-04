62b3c03d53d2704680b9cbf7235fee5a93f86f79
e2tha-e <hyunhoee@gmail.com>
2015-10-04 19:10:46 -0400

    stopping point

--- backend/wordpress/wp-content/themes/fepper/fp-header.php
+++ backend/wordpress/wp-content/themes/fepper/fp-header.php
@@ -1,9 +1,29 @@
 <!-- Begin .header -->
 <header class="header cf" role="banner">
-	{{> atoms-logo }}
-	<a href="#" class="nav-toggle nav-toggle-search icon-search"><span class="is-vishidden">Search</span></a>
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo.png" class="logo" alt="Logo Alt Text" /></a>
+<a href="#" class="nav-toggle nav-toggle-search icon-search"><span class="is-vishidden">Search</span></a>
 	<a href="#" class="nav-toggle nav-toggle-menu icon-menu"><span class="is-vishidden">Menu</span></a>
-	{{> molecules-primary-nav }}
-	{{> molecules-search }}
+<nav id="nav" class="nav">
+	<ul>
+		<li><a href="">Home</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="">Blog</a></li>
+		<li><a href="#">Contact</a></li>
+	</ul>
+</nav><!--end .nav-->
+
+<form action="#" method="post" class="inline-form search-form">
+	<fieldset>
+		<legend class="is-vishidden">Search</legend>
+		<label for="search-field" class="is-vishidden">Search</label>
+		<input type="search" placeholder="Search" id="search-field" class="search-field" />
+		<button class="search-submit">
+			<span class="icon-search" aria-hidden="true"></span>
+			<span class="is-vishidden">Search</span>
+		</button>
+	</fieldset>
+</form>
+
 </header>
 <!-- End .header -->
+
