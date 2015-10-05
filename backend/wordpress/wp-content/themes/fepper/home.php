80b6432247d814ef63330f9a3e0be00ac68ed6c5
e2tha-e <hyunhoee@gmail.com>
2015-10-05 19:09:15 -0400

    templates

--- backend/wordpress/wp-content/themes/fepper/home.php
+++ backend/wordpress/wp-content/themes/fepper/home.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Fepper
- */
-
 get_header();
-
 get_template_part( 'fp-home' );
-
 get_footer();
 ?>
--- backend/wordpress/wp-content/themes/fepper/single.php
+++ backend/wordpress/wp-content/themes/fepper/single.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * The template for displaying all single posts and attachments
- *
- * @package WordPress
- * @subpackage Fepper
- */
-
 get_header();
-
 get_template_part( 'fp-single' );
-
 get_footer();
 ?>
