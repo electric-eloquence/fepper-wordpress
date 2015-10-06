11e3d7ef8ffc124c00aa567f1ac702cc815cc448
e2tha-e <hyunhoee@gmail.com>
2015-10-06 13:13:03 -0400

    wordpress styled

--- backend/wordpress/wp-content/themes/fepper/fp-header.php
+++ backend/wordpress/wp-content/themes/fepper/fp-header.php
@@ -1,13 +1,11 @@
 <!-- Begin .header -->
 <header class="header cf" role="banner">
 	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo.png" class="logo" alt="Logo Alt Text" /></a>
-<a href="#" class="nav-toggle nav-toggle-search icon-search"><span class="is-vishidden">Search</span></a>
-	<a href="#" class="nav-toggle nav-toggle-menu icon-menu"><span class="is-vishidden">Menu</span></a>
-<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+<?php wp_nav_menu( array( 'menu_class' => 'nav', 'theme_location' => 'primary' ) ); ?>
+	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
   <div id="widget-area" class="widget-area" role="complementary">
     <?php dynamic_sidebar( 'sidebar' ); ?>
   </div><!-- .widget-area -->
 <?php endif; ?>
-	<?php wp_nav_menu( array( 'menu_class' => 'nav', 'theme_location' => 'primary' ) ); ?>
 </header>
 <!-- End .header -->
