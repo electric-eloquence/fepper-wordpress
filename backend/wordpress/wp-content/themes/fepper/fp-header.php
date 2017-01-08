864c5b311e9ca6f89cec00ad65c71b37b144b30c
e2tha-e <ee@electric-eloquence.io>
2017-01-07 19:10:00 -0500

    prefixing assets, scripts, and styles dirs with underscores

--- backend/wordpress/wp-content/themes/fepper/fp-header.php
+++ backend/wordpress/wp-content/themes/fepper/fp-header.php
@@ -1,6 +1,6 @@
 <!-- Begin .header -->
 <header class="header cf" role="banner">
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/src/logo.png" class="logo" alt="Logo Alt Text" /></a>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/_assets/src/logo.png" class="logo" alt="Logo Alt Text" /></a>
 <a href="#" class="nav-toggle nav-toggle-search icon-search"><span class="is-vishidden">Search</span></a>
 	<a href="#" class="nav-toggle nav-toggle-menu icon-menu"><span class="is-vishidden">Menu</span></a>
 	<?php wp_nav_menu( array( 'menu_class' => 'nav', 'theme_location' => 'primary' ) ); ?>
