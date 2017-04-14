bc1840a78ffa7a1952257963691c764110bca65f
e2tha-e <ee@electric-eloquence.io>
2017-04-14 10:51:13 -0400

    nav and search styles

--- backend/wordpress/wp-content/themes/fepper/fp-header.php
+++ backend/wordpress/wp-content/themes/fepper/fp-header.php
@@ -3,11 +3,11 @@
 	<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/_assets/src/logo.png" class="logo" alt="Logo Alt Text" /></a>
 <a href="#" class="nav-toggle nav-toggle-search icon-search"><span class="is-vishidden">Search</span></a>
 	<a href="#" class="nav-toggle nav-toggle-menu icon-menu"><span class="is-vishidden">Menu</span></a>
-	<?php wp_nav_menu( array( 'menu_class' => 'nav', 'theme_location' => 'primary' ) ); ?>
 	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 	<div id="widget-area" class="widget-area" role="complementary">
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</div><!-- .widget-area -->
 <?php endif; ?>
+	<?php wp_nav_menu( array( 'menu_class' => 'nav', 'theme_location' => 'primary' ) ); ?>
 </header>
 <!-- End .header -->
