083e65790e3b0d7efaca7475b0494800b2e555a8
e2tha-e <ee@electric-eloquence.io>
2017-06-22 10:15:09 -0400

    Squashed commit of the following:
    commit 7aead9535a8897e644cbccc8d13256242d76b9e5
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Thu Jun 22 10:14:03 2017 -0400
    
        style refactor
    
    commit 8e8f063b325337aa8dd0e992146bd2b569488e81
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Thu Jun 22 10:10:12 2017 -0400
    
        header style updates
    
    commit 216b541e7243e8febc4a5856e733ad975f4d1497
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Thu Jun 22 09:27:55 2017 -0400
    
        pattern refactor
    
    commit 26bd93d0de24574e09dde4b808db19073f6de6cc
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Thu Jun 22 08:06:26 2017 -0400
    
        template updates
    
    commit 3e4fa979038010a65edea0c9280b731a368f617b
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Thu Jun 22 07:24:09 2017 -0400
    
        refactored widgets
    
    commit 96e9d2a86af6e2dfaa00d26bea15eb060cdf900b
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Wed Jun 21 21:16:52 2017 -0400
    
        pattern rearrange
    
    commit 396de5445c647b3915cfbe945658fe84434e752a
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Wed Jun 21 18:32:59 2017 -0400
    
        refactored lists
    
    commit 554336caac9443447e7d4ea84036c593465e4c49
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Jun 16 08:43:54 2017 -0400
    
        logo in new install
    
    commit 50e41a657afd585f8b478a13b687621f1ed660bf
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Jun 16 08:37:19 2017 -0400
    
        single-quotes around keys in yaml files
    
    commit 220f49c450b193df473596298558af704821e3e3
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Jun 16 08:28:35 2017 -0400
    
        new install page

--- backend/wordpress/wp-content/themes/fepper/fp-home.php
+++ backend/wordpress/wp-content/themes/fepper/fp-home.php
@@ -19,7 +19,7 @@
 		<h2 class="headline"><?php the_title(); ?></h2>
 		<div class="b-thumb">
 			<?php echo get_the_post_thumbnail( $post, 'medium' ); ?>
-		</div>
+			</div>
 		<div class="b-text">
 			<?php the_excerpt(); ?>
 		</div>
