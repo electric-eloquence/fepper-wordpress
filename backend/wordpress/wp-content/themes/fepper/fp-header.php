7f3c198c86fa02aac873e8b75b8576048e3fd57b
e2tha-e <ee@electric-eloquence.io>
2017-06-25 08:18:24 -0400

    text-decoration on anchor elements

--- backend/wordpress/wp-content/themes/fepper/fp-header.php
+++ backend/wordpress/wp-content/themes/fepper/fp-header.php
@@ -1,10 +1,11 @@
 <!-- Begin .header -->
 <header class="header cf" role="banner">
 	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo">
-		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/_assets/src/logo.svg" alt="<?php echo get_bloginfo(); ?>" />
+	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/_assets/src/logo.svg" alt="<?php echo get_bloginfo(); ?>" />
 
-	</a>
-<a href="#" class="nav-toggle nav-toggle-search icon-search"></a>
+</a>
+
+	<a href="#" class="nav-toggle nav-toggle-search icon-search"></a>
 	<a href="#" class="nav-toggle nav-toggle-menu icon-menu"></a>
 	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 		<div id="widget-area" class="widget-area" role="complementary">
