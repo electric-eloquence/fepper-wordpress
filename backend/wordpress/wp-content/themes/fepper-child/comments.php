b2ae77d0646076ae435746b9cefe0c898c465c12
e2tha-e <ee@electric-eloquence.io>
2017-09-08 06:43:38 -0400

    Squashed commit of the following:
    commit a023becb97f07d3bad740b220565cec15e6e1af0
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Sep 8 06:31:01 2017 -0400
    
        wp theme version bump
    
    commit 6a7771fba1fe01db72a191a62394ccc3c339f27f
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Fri Sep 8 06:26:05 2017 -0400
    
        readme updates
    
    commit 9bfe0355c7b1c60299b01842cc4e6114b424cac3
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Thu Sep 7 20:58:57 2017 -0400
    
        fixed indentation
    
    commit 168e088f58dc9e2452b1385bc27aa589e2330786
    Author: e2tha-e <ee@electric-eloquence.io>
    Date:   Thu Sep 7 20:31:56 2017 -0400
    
        readme update

--- backend/wordpress/wp-content/themes/fepper-child/comments.php
+++ backend/wordpress/wp-content/themes/fepper-child/comments.php
@@ -1,19 +1,19 @@
 <section class="comments section">
 	<?php if ( have_comments() ) : ?>
 		<h2 class="section-title"><?php
-				$comments_number = get_comments_number();
-				printf(
-					_nx(
-						'%1$s comment on &ldquo;%2$s&rdquo;',
-						'%1$s comments on &ldquo;%2$s&rdquo;',
-						$comments_number,
-						'comments title',
-						'fepper'
-					),
-					number_format_i18n( $comments_number ),
-					get_the_title()
-				);
-			?></h2>
+			$comments_number = get_comments_number();
+			printf(
+				_nx(
+					'%1$s comment on &ldquo;%2$s&rdquo;',
+					'%1$s comments on &ldquo;%2$s&rdquo;',
+					$comments_number,
+					'comments title',
+					'fepper'
+				),
+				number_format_i18n( $comments_number ),
+				get_the_title()
+			);
+		?></h2>
 		<?php the_comments_navigation(); ?>
 		<ul class="comment-list">
 			<?php
@@ -27,12 +27,12 @@
 		<?php the_comments_navigation(); ?>
 	<?php endif; ?>
 	<?php
-			// If comments are closed and there are comments, let's leave a little note, shall we?
-			if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-		?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'fepper' ); ?></p>
-		<?php else: ?>
-			<?php comment_form(); ?>
-		<?php endif; ?>
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+	<p class="no-comments"><?php _e( 'Comments are closed.', 'fepper' ); ?></p>
+	<?php else: ?>
+		<?php comment_form(); ?>
+	<?php endif; ?>
 </section>
 
--- backend/wordpress/wp-content/themes/fepper-child/template-parts/fp-single.php
+++ backend/wordpress/wp-content/themes/fepper-child/template-parts/fp-single.php
@@ -11,49 +11,49 @@
 					<header class="article-header">
 						<h1><?php the_title(); ?></h1>
 						<div class="byline">
-								<?php echo __( 'By', 'fepper' ) . ' ' . get_the_author() . ' '; ?>
-								<?php echo __( 'on', 'fepper' ) . ' ' . get_the_date(); ?>
-							</div>
+							<?php echo __( 'By', 'fepper' ) . ' ' . get_the_author() . ' '; ?>
+							<?php echo __( 'on', 'fepper' ) . ' ' . get_the_date(); ?>
+						</div>
 					</header>
 					<?php the_content(); ?>
 				</article><!--end .article-->
 				<div class="page-links">
-						<?php
-							wp_link_pages( array(
-								'before'      => '<span class="page-links-title">' . __( 'Pages:', 'fepper' ) . '</span>',
-								'after'       => '',
-								'link_before' => '<span class="page-link">',
-								'link_after'  => '</span>',
-								'separator'   => ' | ',
-							) );
-						?>
-					</div>
-				<?php
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
+					<?php
+						wp_link_pages( array(
+							'before'      => '<span class="page-links-title">' . __( 'Pages:', 'fepper' ) . '</span>',
+							'after'       => '',
+							'link_before' => '<span class="page-link">',
+							'link_after'  => '</span>',
+							'separator'   => ' | ',
+						) );
 					?>
-			<?php endwhile; ?>
-			<?php
-					if ( is_singular( 'attachment' ) ) :
-						// Parent post navigation.
-						the_post_navigation( array(
-							'prev_text' => _x(
-								'<span class="meta-nav">Published in</span><span class="post-title">%title</span>',
-								'Parent post link',
-								'fepper'
-							),
-						) );
-					elseif ( is_singular( 'post' ) ) :
-						// Previous/next post navigation.
-						the_post_navigation( array(
-							'prev_text' => '<span class="meta-nav">' . __( 'Previous post:', 'fepper' ) . '</span> ' .
-								'<span class="post-title">%title</span>',
-							'next_text' => '<span class="meta-nav">' . __( 'Next post:', 'fepper' ) . '</span> ' .
-								'<span class="post-title">%title</span>',
-						) );
+				</div>
+				<?php
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
 					endif;
 				?>
+			<?php endwhile; ?>
+			<?php
+				if ( is_singular( 'attachment' ) ) :
+					// Parent post navigation.
+					the_post_navigation( array(
+						'prev_text' => _x(
+							'<span class="meta-nav">Published in</span><span class="post-title">%title</span>',
+							'Parent post link',
+							'fepper'
+						),
+					) );
+				elseif ( is_singular( 'post' ) ) :
+					// Previous/next post navigation.
+					the_post_navigation( array(
+						'prev_text' => '<span class="meta-nav">' . __( 'Previous post:', 'fepper' ) . '</span> ' .
+							'<span class="post-title">%title</span>',
+						'next_text' => '<span class="meta-nav">' . __( 'Next post:', 'fepper' ) . '</span> ' .
+							'<span class="post-title">%title</span>',
+					) );
+				endif;
+			?>
 		</div><!--end l-main-->
 
 		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
